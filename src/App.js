d0d1e923f432d44fc284907a5e878d3e6bdebfd0
u24e10 <ivan.rubil@avl.com>
2020-04-18 23:39:08 +0200

    txt change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ev
+          ev promjena Feature branch
         </p>
         <a
           className="App-link"
